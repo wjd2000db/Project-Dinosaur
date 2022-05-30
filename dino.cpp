7ddd66f49fc9320b31db42797649601fde22134f
Yujin <wjd2000db@gmail.com>
2022-05-30 16:43:15 -0400

    moving legs, draw tree

--- dino.cpp
+++ dino.cpp
@@ -1,42 +1,115 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#include <time.h>
 
 
 void legs(void);
 void gotoxy(int x, int y);
 void character(void);
+void CursorView();
+void console(void);
+void DrawMap(void);
+void DrawTree();
+
+#define characterX 10
+#define characterY 12
 
 int main(void)
 {
-	
+	console();
+	DrawMap();
 	character();
-	legs();
+	
 	return 0;
 }
 
 void character(void)
 {	
+	gotoxy(characterX, characterY); 		printf("    ^ㅡㅡㅡ^\n");
+	gotoxy(characterX, characterY+1);		printf("   /  -  - |");
+	gotoxy(characterX, characterY+2);		printf("   |    ^  /");
+	gotoxy(characterX, characterY+3);		printf("   /       ＼");
+	gotoxy(characterX, characterY+4);		printf("  / /     ＼＼");
+	gotoxy(characterX, characterY+5);		printf("  ㅡ|     | ㅡ");
+	gotoxy(characterX, characterY+6);		printf("    |     |");
 
-	gotoxy(10, 12); 	printf("    ^ㅡㅡㅡ^ \n");
-	gotoxy(10, 13);		printf("   /  -  - |\n");
-	gotoxy(10, 14);		printf("   |    ^  /\n");
-	gotoxy(10, 15);		printf("   /       ＼\n ");
-	gotoxy(10, 16);		printf("  / /     ＼＼\n");
-	gotoxy(10, 17);		printf("  ㅡ|     | ㅡ\n");
-	gotoxy(10, 18);		printf("    |     |\n");
-
+	legs();
 
 }
 
 void legs(void)
 {
-	printf("    | |-| |\n");
-	printf("    |_| |_|  \n");
+
+	bool run = true;
+	while (1)
+	{
+		CursorView();
+		if (run == true)
+		{
+			gotoxy(characterX, characterY + 7);		printf("    | |-| |");
+			gotoxy(characterX, characterY + 8);		printf("        |_|");
+			Sleep(130);
+			run = false;
+		}
+		else
+		{
+			gotoxy(characterX, characterY + 7); printf("    | |-| |");
+			gotoxy(characterX, characterY + 8);	printf("    |_|    ");
+			Sleep(130);
+			run = true;
+		}
+
+
+	}
+
+
 }
 
-void gotoxy(int x, int y) {
-	//x, y 좌표 설정
+void eraseLegs(void)
+{
+	gotoxy(characterX, characterY+7);		printf("        \n");
+	gotoxy(characterX, characterY + 8);		printf("        \n");
+}
+
+void gotoxy(int x, int y) 
+{
 	COORD pos = { x,y };
-	//커서 이동
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int time()
+{
+	for (;;) 
+	{
+		clock_t time = clock();
+		double timeSecond = (double)(time / CLOCKS_PER_SEC);
+	}
+}
+
+void CursorView(void)
+{
+	CONSOLE_CURSOR_INFO cursorInfo = { 0, };
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+void console(void)
+{
+	system("mode con:cols=90 lines=25");
+	SetConsoleTitle(TEXT("Run Run!"));
+}
+
+void DrawMap()
+{
+	gotoxy(0, 21);		printf("==========================================================================================\n");
+	DrawTree();
+}
+
+void DrawTree()
+{
+	gotoxy(86, 17);		printf("***\n");
+	gotoxy(86, 18);		printf("*|* \n");
+	gotoxy(86, 19);		printf(" | \n");
+	gotoxy(86, 20);		printf(" | \n");
 }
